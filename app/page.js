0da7efff5dd857f96dea4471a34eff3b9d885bd3
Joseph Antony Pierre Avila Foure - Tismart <extjpierre@belcorp.biz>
2025-02-03 16:50:17 -0500

    funcionalidad tecla C y nueva operacion

--- app/page.js
+++ app/page.js
@@ -1,22 +1,23 @@
-'use client'
+"use client";
 import React, { useState } from "react";
-import Buttons from './components/Buttons.jsx';
-import Display from './components/display.jsx';
+import Buttons from "./components/Buttons.jsx";
+import Display from "./components/display.jsx";
 
 export default function Home() {
-
-
-  const [input, setInput] = useState(""); // Almacena la entrada actual
+  const [input, setInput] = useState("");
   const [operator, setOperator] = useState(null);
   const [prevValue, setPrevValue] = useState(null);
+  const [isResult, setIsResult] = useState(false);
 
   const handleButtonClick = (value) => {
     if (!isNaN(value) || value === ".") {
-      setInput((prev) => prev + value);
+      setInput(isResult ? value : input + value);
+      setIsResult(false);
     } else if (["+", "-", "x", "/"].includes(value)) {
       setPrevValue(input);
       setInput("");
       setOperator(value);
+      setIsResult(false);
     } else if (value === "=") {
       if (prevValue !== null && input !== "") {
         const num1 = parseFloat(prevValue);
@@ -43,11 +44,16 @@ export default function Home() {
         setInput(result.toString());
         setPrevValue(null);
         setOperator(null);
+        setIsResult(true);
       }
     } else if (value === "AC") {
       setInput("");
       setPrevValue(null);
       setOperator(null);
+      setIsResult(false);
+    } else if (value === "C") {
+      setInput("");
+      setIsResult(false);
     }
   };
 
@@ -55,9 +61,9 @@ export default function Home() {
     <div className="page w-full h-full flex-col flex justify-center items-center">
       <h1 className="text-4xl font-bold mt-10">Esto es una calculadora</h1>
       <div className="body w-80 bg-slate-400 mt-10 rounded-2xl shadow-slate-400 shadow-md">
-      <Display total={input} />
-      <Buttons onClick={handleButtonClick} />
+        <Display total={input} />
+        <Buttons onClick={handleButtonClick} />
       </div>
     </div>
-    );
+  );
 }
